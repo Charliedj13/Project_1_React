1379a470c527c8a6c6507c7c668cc769c0bb6924
Charlie Jenkins <charliedj13@gmail.com>
2023-02-21 22:17:04 -0600

    final update

---
+++ src/components/Portfolio.js
@@ -0,0 +1,55 @@
+import React from "react"
+
+const Portfolio = () => {
+    return (
+        <div>
+            {/* <header id="header">
+        <h1>
+            Charles Denver Jenkins
+        </h1>
+        <nav id="links">
+            <a href="index.html">Home</a>
+            <a href="about.html">About</a>
+            <a href="portfolio.html">Portfolio</a>
+        </nav>
+    </header> */}
+    <h2 id="title">
+        Projects I've created
+    </h2>
+    <section id="section1">
+        <h2>
+         First Project
+        </h2>
+        <img src="https://thumbs.dreamstime.com/b/projects-lead-us-to-success-our-personal-professional-life-necessary-reach-our-goals-everyday-92274762.jpg"alt="project"/>
+        <p>
+        Lorem ipsum dolor sit amet, ad dolor discere eam, molestie maluisset vim no. Exerci admodum ad usu, qui sint solum quando ne. Adhuc nullam per ex, eam ex pertinax iudicabit, sed cu decore reformidans. In eos laudem saperet, vim prima accusamus no. Discere cotidieque no vel, id dico vocibus qui.
+
+            Te vide elitr eam, oblique alterum per an. Tritani referrentur efficiantur an mei, sonet recusabo assentior per ea. Sit libris inimicus persecuti et, eu quod natum per. Ei munere melius fierent quo, facer munere aperiam eu per. An viderer placerat oportere eam, iisque fabellas in est, erroribus incorrupte vim ei. Veri persius liberavisse mel ei, mea id option sententiae temporibus. Unum magna id eam, ludus dictas blandit cu duo.
+        </p>
+    </section>
+    <section id="section2">
+        <h2>
+            Second Project
+        </h2>
+        <img src="https://thumbs.dreamstime.com/b/projects-lead-us-to-success-our-personal-professional-life-necessary-reach-our-goals-everyday-92274762.jpg"alt="project"/>
+        <p>
+            Lorem ipsum dolor sit amet, ad dolor discere eam, molestie maluisset vim no. Exerci admodum ad usu, qui sint solum quando ne. Adhuc nullam per ex, eam ex pertinax iudicabit, sed cu decore reformidans. In eos laudem saperet, vim prima accusamus no. Discere cotidieque no vel, id dico vocibus qui.
+
+    Te vide elitr eam, oblique alterum per an. Tritani referrentur efficiantur an mei, sonet recusabo assentior per ea. Sit libris inimicus persecuti et, eu quod natum per. Ei munere melius fierent quo, facer munere aperiam eu per. An viderer placerat oportere eam, iisque fabellas in est, erroribus incorrupte vim ei. Veri persius liberavisse mel ei, mea id option sententiae temporibus. Unum magna id eam, ludus dictas blandit cu duo.
+        </p>
+    </section>  
+    <section id="section3">
+        <h2>
+            Third Project
+        </h2>
+        <img src="https://thumbs.dreamstime.com/b/projects-lead-us-to-success-our-personal-professional-life-necessary-reach-our-goals-everyday-92274762.jpg"alt="project"/>
+        <p>
+            Lorem ipsum dolor sit amet, ad dolor discere eam, molestie maluisset vim no. Exerci admodum ad usu, qui sint solum quando ne. Adhuc nullam per ex, eam ex pertinax iudicabit, sed cu decore reformidans. In eos laudem saperet, vim prima accusamus no. Discere cotidieque no vel, id dico vocibus qui.
+
+    Te vide elitr eam, oblique alterum per an. Tritani referrentur efficiantur an mei, sonet recusabo assentior per ea. Sit libris inimicus persecuti et, eu quod natum per. Ei munere melius fierent quo, facer munere aperiam eu per. An viderer placerat oportere eam, iisque fabellas in est, erroribus incorrupte vim ei. Veri persius liberavisse mel ei, mea id option sententiae temporibus. Unum magna id eam, ludus dictas blandit cu duo.
+        </p>
+    </section>
+        </div>
+    )
+}
+export default Portfolio;
--- src/index.js
+++ src/index.js
@@ -2,18 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Homepage from "./components/Homepage";
 import "./components/Homepage.css"
-import Mission from "./components/Mission";
-import "./components/Mission.css";
-import Quote from "./components/Quote";
-import "./components/Quote.css"
+import Aboutme from "./components/Aboutme"
+import "./components/Aboutme.css";
+import Portfolio from "./components/Portfolio"
+import "./components/Portfolio.css"
+
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 const MainComponent = () => {
     return (
+        <BrowserRouter>
     <div>
-        <Homepage />
-        <Mission />
-        <Quote />
+        <header id="header">
+            <h3>
+                Charles Jenkins
+            </h3>
+            <h3>
+            Creativity, Dedication, Excellence
+        </h3>
+        </header>
+        <nav id="links">
+            <Link to="/">Homepage</Link>
+            <Link to="/About-me">About Me</Link>
+            <Link to="/Portfolio">Portfolio</Link>
+        </nav>
+        <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/About-me" element={<Aboutme />} />
+        <Route path="/Portfolio" element={<Portfolio />} />
+        </Routes>
     </div>
+    </BrowserRouter>
 )}
 
 
